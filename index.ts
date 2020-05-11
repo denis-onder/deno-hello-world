6badbb3f57f0f93e09ec05afbf83bae5ec40949c
Denis Onder <denis_onder@protonmail.com>
2020-05-11 23:24:29 +0200

    Simple HTTP server serving static HTML files

--- index.ts
+++ index.ts
@@ -1,1 +1,18 @@
-console.log("Hello world!");
+import { listenAndServe } from "https://deno.land/std/http/server.ts";
+
+const port = 9090;
+
+listenAndServe({ port }, async (req) => {
+  console.log("Inbound request");
+  if (req.method === "GET" && req.url === "/") {
+    req.respond({
+      status: 200,
+      headers: new Headers({
+        "content-type": "text/html",
+      }),
+      body: await Deno.open("./serve_me.index.html"),
+    });
+  }
+});
+
+console.log(`Server running\nhttp://localhost:${port}/`);
